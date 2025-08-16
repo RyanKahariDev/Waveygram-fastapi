27445874ec057c8ce72eb511b4647d48c723e541
RyanKahariDev <kahariryan878@gmail.com>
2025-08-16 13:19:58 +0200

    url fix

--- app/database.py
+++ app/database.py
@@ -13,6 +13,12 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set")
+
+if "sslmode" not in DATABASE_URL:
+    if "?" in DATABASE_URL:
+        DATABASE_URL += "&sslmode=require"
+    else:
+        DATABASE_URL += "?sslmode=require"
 # import psycopg2
 # from psycopg2.extras import RealDictCursor
 # import time
